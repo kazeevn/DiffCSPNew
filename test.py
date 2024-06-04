367f0caef1b42291f048d1b533ebfe5c9eb97043
RomanovIgnat <romanov.ignat.p@gmail.com>
2024-06-04 17:43:32 +0300

    fix

--- test.py
+++ test.py
@@ -67,7 +67,7 @@ for i in tqdm(range(len(num_atoms))):
 def process_pair(s1, s2):
     if not structure_validity(s1) or not structure_validity(s2):
         return None
-    d = matcher.get_rms_dist(s1, s2)
+    d = matcher.get_rms_dist(s1, s2)[0]
     if d is None:
         return None
     return d
